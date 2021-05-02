222c4c19a29cc0c3dd98df713f102c4bcf302070
Artur <drugartur5@gmail.com>
2021-05-02 12:07:40 +0300

    add basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/wines'>
             <Application/>
         </BrowserRouter>
     );
